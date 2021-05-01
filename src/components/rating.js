049d1451b2159f88024184d4dc28c5f50905f914
אור ברמן <orber3@gmail.com>
2021-05-01 10:36:25 +0300

    finished rating test

--- src/components/rating.js
+++ src/components/rating.js
@@ -9,44 +9,44 @@ const Rating = ({value , text,}) => {
 <div> 
 <span> 
              {value  >= 1 
-                ? <StarIcon style={{color: 'yellow'}} />
+                ? <StarIcon style={{color: 'yellow'}} data-test="fullStar" />
                 : value >= 0.5 
-                 ?<StarHalfIcon />
-                  :<StarBorderIcon />
+                 ?<StarHalfIcon data-test="halfStar"/>
+                  :<StarBorderIcon data-test="borderStar"/>
                  } 
 
             </span>
             <span> 
                 {value >= 2 
-                ? <StarIcon style={{color: 'yellow'}} />
+                ? <StarIcon style={{color: 'yellow'}} data-test="fullStar" />
                 : value >= 1.5 
-                 ?<StarHalfIcon />
-                  :<StarBorderIcon />
+                 ?<StarHalfIcon data-test="halfStar"/>
+                  :<StarBorderIcon data-test="borderStar" />
                  } 
             </span>
             <span> 
              {value >= 3 
-                ? <StarIcon style={{color: 'yellow'}} />
+                ? <StarIcon style={{color: 'yellow'}} data-test="fullStar" />
                 : value >= 2.5 
-                 ?<StarHalfIcon />
-                  :<StarBorderIcon />
+                 ?<StarHalfIcon  data-test="halfStar"/>
+                  :<StarBorderIcon data-test="borderStar" />
                  } 
             </span>
             <span> 
           {
             value >= 4
-              ? <StarIcon style={{color: 'yellow'}} />
+              ? <StarIcon style={{color: 'yellow'}} data-test="fullStar" />
               : value >= 3.5
-              ? <StarHalfIcon />
-              : <StarBorderIcon />
+              ? <StarHalfIcon data-test="halfStar" />
+              : <StarBorderIcon data-test="borderStar" />
           }
             </span>
             <span> 
                {value >= 5 
-                ? <StarIcon style={{color: 'yellow'}} />
+                ? <StarIcon style={{color: 'yellow'}} data-test="fullStar"  />
                 : value >= 4.5 
-                 ?<StarHalfIcon />
-                  :<StarBorderIcon />
+                 ?<StarHalfIcon data-test="halfStar" />
+                  :<StarBorderIcon data-test="borderStar" />
                  } 
             </span>
             <span>
--- src/components/rating.test.js
+++ src/components/rating.test.js
@@ -1,27 +1,44 @@
 import {findByTestAtrr , checkProps, storeFactory , findFirstAttr,findHookAttr} from '../utills/testUtils'
 import Rating from './rating'
 import { shallow,mount } from 'enzyme'
-
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
 
 const setUp =( props, initialState={}) => { 
 
     const store = storeFactory(initialState)
     return shallow(
-        <Provider store={store}>
+        
        <Rating {...props} />
-         </Provider> )
+         )
 }
 
 
 describe('should render staricons according to value ', () => { 
 
 
-    test('value equal 1 ,should render 1 star icon' , () => { 
 
-        const wrapper = setUp(value=1)
-        const span = wrapper.find('span')
-        // const RList = findByTestAtrr(wrapper, 'recipe-list');
-        expect(span.length).toBe(1)
+    test('value equal 1 ,should render 1 star icon' , () => { 
+        const props = { 
+            value: 1
+        }
+        const wrapper = setUp(props)
+
+
+        const fullstar = findByTestAtrr(wrapper, 'fullStar');
+  
+        expect(fullstar.length).toBe(1)
+        
+        const halfStar = findByTestAtrr(wrapper, 'halfStar');
+  
+        expect(halfStar.length).toBe(0)
+          
+        const borderStar = findByTestAtrr(wrapper, 'borderStar');
+  
+        expect(borderStar.length).toBe(4)
+
+
     })
 
 
